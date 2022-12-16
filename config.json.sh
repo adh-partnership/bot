383ce2f61b1e69a1bff5b252b260071dbb38e305
Daniel Hawton <daniel@hawton.org>
2022-12-15 21:17:18 -0700

    decode config from secret

--- config.json.sh
+++ config.json.sh
@@ -1,23 +1,3 @@
 #!/bin/sh
 
-cat >config.json <<!SUB!THIS!
-{
-  "discord": {
-    "appid": "$DISCORD_APPID",
-    "guild": "$DISCORD_GUILD",
-    "token": "$DISCORD_TOKEN"
-  },
-  "github": {
-    "token": "$GITHUB_TOKEN"
-  },
-  "facility": {
-    "api_url": "$FACILITY_API",
-    "roster_api": "$ROSTER_API",
-    "roles": {
-      "visitor": "$VISITOR_ROLE",
-      "member": "$MEMBER_ROLE",
-      "guest": "$GUEST_ROLE"
-    }
-  }
-}
-!SUB!THIS!
+echo $BOT_CONFIG | base64 -d > ./config.json
